7a8383f296b3472bffb7dc292c849422db00b334
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-07-19 23:03:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,35 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timestring) {
+    let time = timestring.split(":");
+    let hours = parseInt(time[0]);
+
+    if (hours < 12) {
+
+        return "Good Morning";
+
+    } else if (hours >= 12 && hours < 17) {
+
+        return "Good Afternoon";
+
+    } else if (hours >= 17) {
+
+        return "Good Evening";
+    }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greet) {
+
+    document.getElementById("greeting").innerHTML = greet;
+
+
+
+}
